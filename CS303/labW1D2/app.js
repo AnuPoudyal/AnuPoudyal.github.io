2a54c4f04f6eb7f65c7809c86ceae4d5658e5990
AnuPoudyal <79480470+AnuPoudyal@users.noreply.github.com>
2021-03-23 22:01:38 -0500

    w1d2_solution

--- CS303/labW1D2/app.js
+++ CS303/labW1D2/app.js
@@ -1,23 +1,19 @@
-/*function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
-} */
-
 function nameString(obj) {
-    if(obj===null){
-return "null argument";
-}
-if(obj===undefined || obj===null){
-	return "no object passes";
-}
-if(obj.length===0){
-	return "object has no properties";
-}
-let arr=[];
-for(let i in obj){
-	arr.push(i)
-}
-arr.sort();
-let str=arr.join('.')
-return str;
+    if (obj === null) {
+        return "null argument";
+    }
+    if (obj === undefined || obj === null) {
+        return "no object passed";
+    }
+    if (obj.length === 0) {
+        return "object has no properties";
+    }
+    let arr = [];
+    for (let k in obj) {
+        arr.push(k)
+    }
+    arr.sort();
+
+    let str = arr.join('.')
+    return str
 }
