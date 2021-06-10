5a1a540bf5e2510a0e80e39d0bdb4f50b2af7bbc
AnuPoudyal <79480470+AnuPoudyal@users.noreply.github.com>
2021-06-10 17:47:10 -0500

    Update user.js

--- cs445-project/user.js
+++ cs445-project/user.js
@@ -1,6 +1,6 @@
 (async () => {
     // Get User Details
-    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
     console.log('User Details---->', data);
   
@@ -31,7 +31,7 @@
     console.log('USER ID--->', userId);
 
     const post = await fetch(
-      `http://jsonplaceholder.typicode.com/posts/${userId}`
+      `https://jsonplaceholder.typicode.com/posts/${userId}`
     );
     const postData = await post.json();
     console.log('Post Data--->', postData);
@@ -47,7 +47,7 @@
 const showComment = document.getElementById('comment');
 showComment.addEventListener('click', async () => {
   const comment = await fetch(
-    `http://jsonplaceholder.typicode.com/comments?postId=${userId}`
+    `https://jsonplaceholder.typicode.com/comments?postId=${userId}`
   );
   const commentData = await comment.json();
   const commentBody = document.getElementById('commentBody');
